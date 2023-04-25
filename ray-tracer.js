467392e10e3a07cbfdf04158c435427f0bd2df78
bricksphd <bricks@unomaha.edu>
2023-04-24 20:08:20 -0500

    Add comments

--- ray-tracer.js
+++ ray-tracer.js
@@ -20,36 +20,57 @@ function main(scene) {
   let rayTracedObjects = scene.rayTracedObjects
 
   //Ray Tracer starts
+  //Loop over all the pixels
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
+
+      //Debug code
       if (x == 50 && y == 50) {
         console.log("stop")
       }
 
+      //The color of the closest collision for this pixel
       let rayTracedPixel = backgroundColor;
+
+      //The distance to the closest collision for this pixel
       let closestPositiveT = 10000000
 
+      //Determine the origin and direction of the ray
+      let startX = x - width / 2;
+      let startY = y - height / 2;
+      let origin = new Vector3(startX, startY, scene.camera.z);
+      let direction = new Vector3(0, 0, -1);
+
+      //Loop over all the rayTracedObjects in this scene
+      //Note that in an optimized ray tracer, 
+      //you can use spatial subdivision to reduce this from 
+      //O(n) to O(log(n))
       for (let rayTracedObject of rayTracedObjects) {
+        
+        //Get the geometry of the current object
         let geometry = rayTracedObject.geometry
 
-        let startX = x - width / 2;
-        let startY = y - height / 2;
-        let origin = new Vector3(startX, startY, scene.camera.z);
-        let direction = new Vector3(0, 0, -1);
+        //Find the intersection with this object
         let collision = geometry.intersect(origin, direction);
 
-
+        //Check to see if the collision exists...
+        //...and if it is closer than any other collision we've seen
         if (collision && collision.timeToCollision < closestPositiveT) {
+          //Get the distance to collision
           closestPositiveT = collision.timeToCollision
+          
+          //Get the location of the collision
           let c = collision.collisionLocation
           let normal = collision.normalAtCollision
+          
+          //Use the shader to calculate the color at this collision
           rayTracedPixel =  rayTracedObject.shader.illuminateObject(origin, c, normal, [])
         }
 
-        // image.setPixel(x, y, rayTracedPixel);
-
-        // let pixel = image.getPixel(x, y);
+        //Store the color as a string
         let pixelString = `rgb(${rayTracedPixel.r}, ${rayTracedPixel.g}, ${rayTracedPixel.b})`;
+        
+        //Use the canvas to draw on the screen.
         ctx.fillStyle = pixelString;
         ctx.fillRect(x, y, 1, 1);
       }
--- triangle.js
+++ triangle.js
@@ -1,33 +1,75 @@
+/**
+ * A triangle class, with vertices specified in counter-clockwise order
+ */
 class Triangle{
+  /**
+   * 
+   * @param {Vector3} one The first vertex
+   * @param {Vector3} two The second vertex
+   * @param {Vector3} three The third vertex
+   */
   constructor(one, two, three){
     this.vertices = [];
     this.vertices.push(...[one, two, three])
   }
+  
+  /**
+   * Get the first vertex
+   */
   get one(){
     return this.vertices[0];
   }
+  
+  /**
+   * Get the second vertex
+   */
   get two(){
     return this.vertices[1]
   }
+  
+  /**
+   * Get the third vertex
+   */
   get three(){
     return this.vertices[2]
   }
+  
+  /**
+   * Get the vector from one to two
+   */
   get oneTwo(){
     return this.one.position.minus(this.two.position).normalize();
   }
-  get twoThree(){
-    return this.two.position.minus(this.three.position).normalize();
-  }
+  
+  /**
+   * Get the vector from three to two
+   */
   get threeTwo(){
     return this.three.position.minus(this.two.position).normalize();
   }
+  
+  /**
+   * Calculate the collision point with the triangle
+   * @param {Vector3} origin The origin of the ray
+   * @param {Vector3} direction The direction of the ray
+   * @returns The cillion point
+   */
   intersect(origin,direction){
     //First find the collision on the plane
+    
+    //Get the perpendicular (normal) of the plane that contains this triangle
     let ABC = this.threeTwo.cross(this.oneTwo).normalize();
+    
+    //Calculate the D from the plane that contains this triangle
     let D = -(ABC.dot(this.one.position));
+
+    //Calculate the time to collision
     let timeToCollision = (-D-origin.dot(ABC))/(direction.dot(ABC))
+    
+    //Calculate the collision location
     let collisionLocation = origin.add(direction.scale(timeToCollision));
-    // return {timeToCollision, collisionLocation};
+    
+    //Return all the details about the collision
     return new Collision(timeToCollision, collisionLocation, ABC);
   }
 }
